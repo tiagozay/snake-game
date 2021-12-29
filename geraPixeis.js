458c228da022f7f437e252a08dd61f7aee2cb640
Tiago zay <tiagozay@gmail.com>
2021-12-29 18:36:42 -0300

    Jogo da minhoca completo

--- geraPixeis.js
+++ geraPixeis.js
@@ -1,10 +1,10 @@
 var tela = document.querySelector(".pixels")
 var x = 1;
 var y = 1;
-for(let i = 1; i <= 3807; i++){
+for(let i = 1; i <= 1056; i++){
     tela.innerHTML += `<div data-x=${x} data-y=${y} class="pixel"></div>`
     x++
-    if(i % 81 == 0){
+    if(i % 44 == 0){
         x = 1;
         y++
     }
--- script.js
+++ script.js
@@ -9,41 +9,76 @@ function pegaUmPixel(x, y){
     return elemento;
 }
 
+var quantidadeDePixelsX = 35;
+var quantidadeDePixelsY = 20;
+
+
+var tamanho = 5;
 
 var movimentoY = 0;
 var movimentoX = 0;
 
 var px = 1;
 var py = 1;
+
+var numeroDoPixel = 0;
+
 setInterval(function(){
-    pegaUmPixel(px, py).style.backgroundColor = 'red'
+    detectaColisaoComAlimento()
+    pixelParaSerPintado = pegaUmPixel(px, py);
+    pixelParaSerPintado.classList.add("pixelPintado")
+    pixelParaSerPintado.classList.add("p"+numeroDoPixel)
+    pixelParaSerPintado.dataset.numero = numeroDoPixel;
     px += movimentoX;
     py += movimentoY;
-    if(px > 81){
+    if(px > 44){
         px = 1;
     }
-    if(py > 47){
+    if(py > 24){
         py = 1;
     }
 
     if(px < 1){
-        px = 81;
+        px = 44;
     }
     if(py < 1){
-        py = 47;
+        py = 24;
     }
+
+
+    var pixels = document.querySelectorAll(".pixelPintado")
+
+    if(pixels.length > tamanho){
+        var pxc = document.querySelector(".p"+(numeroDoPixel -tamanho))
+        
+        pxc.classList.remove('pixelPintado')
+    }
+
+    numeroDoPixel++
+
+    pixels.forEach(function(pixel){
+        if(pixel.classList[1] != "alimento" && pixel.dataset.numero < numeroDoPixel - tamanho){
+            pixel.classList.remove("pixelPintado")
+        }
+    })
+
+    
 }, 50)
 
 
 document.onkeydown = function(){
     if(event.keyCode == 38){
         //cima
+        // movimentoY = -1;
+        // movimentoX = 0;
         if(movimentoY != 1){
             movimentoY = -1;
             movimentoX = 0;
         }
     }else if(event.keyCode == 40){
         //baixo
+        // movimentoY = 1;
+        // movimentoX = 0;
         if(movimentoY != -1){
             movimentoY = 1;
             movimentoX = 0;
@@ -51,6 +86,8 @@ document.onkeydown = function(){
       
     }else if(event.keyCode == 39){
         //direita
+        // movimentoX = 1;
+        // movimentoY = 0;
         if(movimentoX != -1){
             movimentoX = 1;
             movimentoY = 0;
@@ -58,6 +95,8 @@ document.onkeydown = function(){
        
     }else if(event.keyCode == 37){
         //esquerda
+        // movimentoX = -1;
+        // movimentoY = 0;
         if(movimentoX != 1){
             movimentoX = -1;
             movimentoY = 0;
@@ -65,4 +104,24 @@ document.onkeydown = function(){
      
     }
     
+}
+
+var pxAlimento
+var pyAlimento 
+gerarAlimento()
+function gerarAlimento(){
+    pxAlimento = Math.round(Math.random() * (quantidadeDePixelsX - 1) + 1)
+    pyAlimento =  Math.round(Math.random() * (quantidadeDePixelsY - 1) + 1)
+
+    pegaUmPixel(pxAlimento, pyAlimento).classList.add("alimento");
+}
+
+
+function detectaColisaoComAlimento(){
+    console.log(pxAlimento, px)
+    if(pxAlimento == px && pyAlimento == py ){
+        pegaUmPixel(pxAlimento, pyAlimento).classList.remove("alimento");
+        tamanho++
+        gerarAlimento()
+    }
 }
