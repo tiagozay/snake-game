c2bfb6b998ffe8808d4ba4fd4f86bcbbaf2c89b5
Tiago zay <tiagozay@gmail.com>
2021-12-30 10:11:40 -0300

    Mudando a cor de cobra

--- script.js
+++ script.js
@@ -1,3 +1,66 @@
+var quantidadeDePixelsX = 35;
+var quantidadeDePixelsY = 20;
+
+var tamanho = 5;
+
+var movimentoY = 0;
+var movimentoX = 1;
+
+var px = 1;
+var py = 1;
+
+var pixeisGerados = [];
+
+setInterval(function(){
+    detectaColisaoComAlimento()
+    pixelParaSerPintado = pegaUmPixel(px, py);
+    pixelParaSerPintado.classList.add("pixelPintado")
+    pixeisGerados.push(pixelParaSerPintado)
+
+    px += movimentoX;
+    py += movimentoY;
+    detectaColisaoComBordasEMoveParaOutroLado()
+
+    if(pixeisGerados.length > tamanho){
+        pixeisGerados[0].classList.remove("pixelPintado")
+        pixeisGerados.shift();
+    }
+
+}, 100)
+
+
+document.onkeydown = function(){
+    if(event.keyCode == 38){
+        //cima
+        if(movimentoY != 1){
+            movimentoY = -1;
+            movimentoX = 0;
+        }
+    }else if(event.keyCode == 40){
+        //baixo
+        if(movimentoY != -1){
+            movimentoY = 1;
+            movimentoX = 0;
+        }
+      
+    }else if(event.keyCode == 39){
+        //direita
+        if(movimentoX != -1){
+            movimentoX = 1;
+            movimentoY = 0;
+        }
+       
+    }else if(event.keyCode == 37){
+        //esquerda
+        if(movimentoX != 1){
+            movimentoX = -1;
+            movimentoY = 0;
+        }
+     
+    }
+    
+}
+
 function pegaUmPixel(x, y){
     var elementos = document.querySelectorAll(`[data-x='${x}']`)
     var elemento = null;
@@ -9,28 +72,7 @@ function pegaUmPixel(x, y){
     return elemento;
 }
 
-var quantidadeDePixelsX = 35;
-var quantidadeDePixelsY = 20;
-
-
-var tamanho = 5;
-
-var movimentoY = 0;
-var movimentoX = 0;
-
-var px = 1;
-var py = 1;
-
-var numeroDoPixel = 0;
-
-setInterval(function(){
-    detectaColisaoComAlimento()
-    pixelParaSerPintado = pegaUmPixel(px, py);
-    pixelParaSerPintado.classList.add("pixelPintado")
-    pixelParaSerPintado.classList.add("p"+numeroDoPixel)
-    pixelParaSerPintado.dataset.numero = numeroDoPixel;
-    px += movimentoX;
-    py += movimentoY;
+function detectaColisaoComBordasEMoveParaOutroLado(){
     if(px > 44){
         px = 1;
     }
@@ -44,67 +86,8 @@ setInterval(function(){
     if(py < 1){
         py = 24;
     }
-
-
-    var pixels = document.querySelectorAll(".pixelPintado")
-
-    if(pixels.length > tamanho){
-        var pxc = document.querySelector(".p"+(numeroDoPixel -tamanho))
-        
-        pxc.classList.remove('pixelPintado')
-    }
-
-    numeroDoPixel++
-
-    pixels.forEach(function(pixel){
-        if(pixel.classList[1] != "alimento" && pixel.dataset.numero < numeroDoPixel - tamanho){
-            pixel.classList.remove("pixelPintado")
-        }
-    })
-
-    
-}, 50)
-
-
-document.onkeydown = function(){
-    if(event.keyCode == 38){
-        //cima
-        // movimentoY = -1;
-        // movimentoX = 0;
-        if(movimentoY != 1){
-            movimentoY = -1;
-            movimentoX = 0;
-        }
-    }else if(event.keyCode == 40){
-        //baixo
-        // movimentoY = 1;
-        // movimentoX = 0;
-        if(movimentoY != -1){
-            movimentoY = 1;
-            movimentoX = 0;
-        }
-      
-    }else if(event.keyCode == 39){
-        //direita
-        // movimentoX = 1;
-        // movimentoY = 0;
-        if(movimentoX != -1){
-            movimentoX = 1;
-            movimentoY = 0;
-        }
-       
-    }else if(event.keyCode == 37){
-        //esquerda
-        // movimentoX = -1;
-        // movimentoY = 0;
-        if(movimentoX != 1){
-            movimentoX = -1;
-            movimentoY = 0;
-        }
-     
-    }
-    
 }
+
 
 var pxAlimento
 var pyAlimento 
@@ -118,10 +101,15 @@ function gerarAlimento(){
 
 
 function detectaColisaoComAlimento(){
-    console.log(pxAlimento, px)
     if(pxAlimento == px && pyAlimento == py ){
         pegaUmPixel(pxAlimento, pyAlimento).classList.remove("alimento");
         tamanho++
         gerarAlimento()
     }
 }
+
+
+
+var array = ['primeiro', 'segundo', 'terceiro', 'quarto']
+array.shift();
+console.log(array)
